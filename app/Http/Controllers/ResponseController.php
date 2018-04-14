cb12e48e8f0f8c589378fbbaa33c59658f9494a3
Scot Favorite <scot.favorite@gmail.com>
2018-04-14 09:46:47 -0400

    Debug stuff

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -85,7 +85,6 @@ class ResponseController extends Controller
 
     function getLatLon(Request $request) {
         $map_key = env('BING_MAP_KEY');
-        return ($request);
 
         // For now only check for city name
         $queryType = 'citycountrydistrict';
@@ -113,7 +112,8 @@ class ResponseController extends Controller
             default:
                 $res = $client->request('GET', 'http://dev.virtualearth.net/REST/v1/Locations?postalCode=44223&key=' . $map_key);
         }
-
+        return $res->getBody();
+        
         $bingJson = $res->getBody();
         $bingObject = json_decode($bingJson, true);
         \Debugbar::info($bingObject);
