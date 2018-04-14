dd7172e3f250e4e684354a5daf41b1850d10a94c
Scot Favorite <scot.favorite@gmail.com>
2018-04-14 09:51:42 -0400

    Debug stuff

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -104,6 +104,7 @@ class ResponseController extends Controller
             case 'citycountrydistrict':
                 \Debugbar::info($url);
                 //$res = $client->request('GET', 'http://dev.virtualearth.net/REST/v1/Locations/Belize/Belize%20District/Belize%20City/?o=json&key=' . $map_key);
+                return $url;
                 $res = $client->request('GET', $url);
                 break;
             case 'zip':
@@ -112,7 +113,7 @@ class ResponseController extends Controller
             default:
                 $res = $client->request('GET', 'http://dev.virtualearth.net/REST/v1/Locations?postalCode=44223&key=' . $map_key);
         }
-        
+
         $bingJson = $res->getBody();
         $bingObject = json_decode($bingJson, true);
         \Debugbar::info($bingObject);
